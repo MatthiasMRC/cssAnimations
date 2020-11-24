21b3f59a5df0cb65309e512da35f4b575042fbf2
MatthiasMRC <matthias.marchione@gmail.com>
2020-11-24 14:25:44 +0100

    Ajouter de la page AnimationCss et ajout des fonctionnalités page d'accueil

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Typing animation script
 var typed = new Typed(".typing", {
-    strings:["Développeur","Freelancer","Autodidacte"],
+    strings:["Développeur.","Freelancer.","Autodidacte."],
     typeSpeed:30,
     backSpeed:60,
     loop:true,
