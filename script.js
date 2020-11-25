a7a636ae5c7a67a6cfefbdbee56876efd6df145e
MatthiasMRC <matthias.marchione@gmail.com>
2020-11-25 14:42:29 +0100

    amélioration du navbar et modification de la section Button

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+// Modification navbar scrollY 
+
+const nav = document.querySelector('.navbar');
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 30){
+        nav.classList.add('scroll');
+    }else{
+        nav.classList.remove('scroll');
+    }
+});
+
 // Typing animation script
 var typed = new Typed(".typing", {
-    strings:["Développeur.","Freelancer.","Autodidacte."],
+    strings:["Developper.","Freelancer.","Autodidacte."],
     typeSpeed:30,
     backSpeed:60,
     loop:true,
 });
+
